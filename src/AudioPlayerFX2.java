e80f8cf30e9e70cdb376fb2369b70e5c49513b2b
lom <diskedge@yandex.ru>
2016-04-02 09:33:37 +0600

    button

--- src/AudioPlayerFX2.java
+++ src/AudioPlayerFX2.java
@@ -2,6 +2,8 @@
 // works with wav, doesn't work with mp3
 
 import javafx.application.Application;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
@@ -10,21 +12,32 @@ import java.net.URL;
 
 public class AudioPlayerFX2 extends Application {
 
+    Button play;
+
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) {
+
+        primaryStage.setTitle("Audio Player FX");
+
+        play = new Button();
+        play.setText("Play");
+
+
         URL resource = getClass().getResource("back.wav");
         //URL resource = getClass().getResource("work.mp3");
         Media media = new Media(resource.toString());
         MediaPlayer mediaPlayer = new MediaPlayer(media);
         mediaPlayer.play();
 
-        primaryStage.setTitle("AudioPlayerFX2");
-        primaryStage.setWidth(200);
-        primaryStage.setHeight(200);
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(play);
+        primaryStage.setWidth(400);
+        primaryStage.setHeight(250);
         primaryStage.show();
     }
 }
