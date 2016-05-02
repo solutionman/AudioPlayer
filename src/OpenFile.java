af6413f8b1235136c295bb95aa7a166c195aa63a
lom <diskedge@yandex.ru>
2016-05-02 12:50:33 +0600

    FileChooser still not working

--- src/OpenFile.java
+++ src/OpenFile.java
@@ -7,10 +7,20 @@ import java.awt.event.ActionListener;
 
 public class OpenFile implements ActionListener {
     public void actionPerformed(ActionEvent of){
+
+
         System.out.println("openFile button pressed");
         JFileChooser ChooseFile = new JFileChooser();
+        ChooseFile.setCurrentDirectory(new java.io.File("."));
+        ChooseFile.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
         ChooseFile.setSize(200, 200);
         ChooseFile.setVisible(true);
+        ChooseFile.setDialogTitle("Choose the file/folder you want to play");
+
+        if(of.getActionCommand().equals("openFile")){
+            //
+        }
+        System.out.println(ChooseFile.getSelectedFile().getAbsolutePath());
     }
 }
 
