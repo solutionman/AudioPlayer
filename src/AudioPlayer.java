47c19e41bd75b14abf0dcd533629ac3b58a39809
solutionman <solutionman@inbox.ru>
2016-06-11 17:33:20 +0600

    now program not crashing if file not choosen

--- src/AudioPlayer.java
+++ src/AudioPlayer.java
@@ -1,3 +1,4 @@
+
 // Audio Player
 
 import javax.swing.JFrame;
@@ -14,123 +15,116 @@ import java.net.URL;
 
 public class AudioPlayer extends JFrame {
 
-    //public URL url = null;
 	public JFrame BaseWindow;
-    public JLabel playingInfo;
-    public JLabel playlistInfo;
-    public JButton play;
-    public JButton stop;
-    public JButton playlist;
-    public JButton openFile;
-    public String PathToFile = "";
-    public String FileName = "";
+	public JLabel playingInfo;
+	public JLabel playlistInfo;
+	public JButton play;
+	public JButton stop;
+	public JButton playlist;
+	public JButton openFile;
+	public String PathToFile = "";
+	public String FileName = "back.wav";
+	// public String FileName = "runaway.wav";
 
+	public AudioPlayer() {
+		setLayout(new FlowLayout());
 
-    public AudioPlayer() {
-        setLayout(new FlowLayout());
+		playingInfo = new JLabel("Silent for now");
+		add(playingInfo);
 
-        playingInfo = new JLabel("Silent for now");
-        add(playingInfo);
+		play = new JButton("play");
+		add(play);
 
-        play = new JButton("play");
-        add(play);
+		stop = new JButton("stop");
+		add(stop);
 
-        stop = new JButton("stop");
-        add(stop);
+		playlist = new JButton("playlist");
+		add(playlist);
 
-        playlist = new JButton("playlist");
-        add(playlist);
+		playlistInfo = new JLabel("Nothing to show");
+		add(playlistInfo);
 
-        playlistInfo = new JLabel("Nothing to show");
-        add(playlistInfo);
+		openFile = new JButton("openFile");
+		add(openFile);
 
-        openFile = new JButton("openFile");
-        add(openFile);
+		OpenFile of = new OpenFile();
+		openFile.addActionListener(of);
 
-        OpenFile of = new OpenFile();
-        openFile.addActionListener(of);
+		PlayingEvent e = new PlayingEvent();
+		play.addActionListener(e);
+		stop.addActionListener(e);
 
-        PlayingEvent e = new PlayingEvent();
-        play.addActionListener(e);
-        stop.addActionListener(e);
+		PlaylistEvent ex = new PlaylistEvent();
+		playlist.addActionListener(ex);
 
-        PlaylistEvent ex = new PlaylistEvent();
-        playlist.addActionListener(ex);
+	}
 
-    }
+	public class OpenFile implements ActionListener {
+		public void actionPerformed(ActionEvent of) {
 
-    public class OpenFile implements ActionListener {
-        public void actionPerformed(ActionEvent of){
+			try {
+				JFileChooser ChooseFile = new JFileChooser();
+				ChooseFile.setCurrentDirectory(new java.io.File("."));
+				ChooseFile.setSize(200, 200);
+				ChooseFile.setVisible(true);
+				ChooseFile.setDialogTitle("Choose the file/folder you want to play");
 
-            JFileChooser ChooseFile = new JFileChooser();
-            ChooseFile.setCurrentDirectory(new java.io.File("."));
-            //ChooseFile.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-            ChooseFile.setSize(200, 200);
-            ChooseFile.setVisible(true);
-            ChooseFile.setDialogTitle("Choose the file/folder you want to play");
+				int returnValue = ChooseFile.showOpenDialog(null);
+				if (returnValue == JFileChooser.APPROVE_OPTION) {
+					File selectedFile = ChooseFile.getSelectedFile();
+					FileName = selectedFile.getName();
+				}
 
-            int returnValue = ChooseFile.showOpenDialog(null);
-            if(returnValue == JFileChooser.APPROVE_OPTION){
-                File selectedFile = ChooseFile.getSelectedFile();
-                System.out.println(selectedFile.getName());
-                FileName = selectedFile.getName();
-            }
+				PathToFile = ChooseFile.getSelectedFile().getPath();
 
-            //PathToFile = ChooseFile.getSelectedFile().getAbsolutePath();
-            PathToFile = ChooseFile.getSelectedFile().getPath();
-            System.out.println(PathToFile);
-            System.out.println(FileName);
-            //URL url = PlayingEvent.class.getResource(FileName);
-        }
-    }
+			} catch (Exception e) {
+				System.out.println("Choose song");
+			}
 
-    //System.out.println(PathToFile);
-    //System.out.println(FileName);   // Doesn't work here  
-    //URL url = PlayingEvent.class.getResource(FileName);
-    
+		}
+	}
 
-    //URL url = PlayingEvent.class.getResource("/home/dima/JavaProjects/Eclipse/AudoPlayer/AudioPlayer/out/production/AudioPlayer/back.wav");
-    //URL url = PlayingEvent.class.getResource(FileName);
-    URL url = PlayingEvent.class.getResource("back.wav");  
-    
-    AudioClip playing = Applet.newAudioClip(url);
+	URL url = PlayingEvent.class.getResource("" + FileName + ""); // this works
+																	// too
+	// URL url = PlayingEvent.class.getResource("back.wav"); // this works
+	AudioClip playing = Applet.newAudioClip(url);
 
-    
-    public class PlayingEvent implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
+	public class PlayingEvent implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
 
-        	//System.out.println(FileName);  // here it works
-            if(e.getActionCommand().equals("play")){
-                System.out.println("play pressed");
-                playing.play();
-                playingInfo.setText("back.wav");
-            }
-            if(e.getActionCommand().equals("stop")){
-                System.out.println("stop pressed");
-                playing.stop();
-            }
+			System.out.println(FileName); // here it works
+			// System.out.println(PathToFile); // this works too
+			if (e.getActionCommand().equals("play")) {
+				System.out.println("play pressed");
+				playing.play();
+				playingInfo.setText("back.wav");
+			}
+			if (e.getActionCommand().equals("stop")) {
+				System.out.println("stop pressed");
+				playing.stop();
+			}
 
-        }
-    }
+		}
+	}
 
-    public class PlaylistEvent implements ActionListener {
-        public void actionPerformed(ActionEvent ex) {
-            playlistInfo.setText("Here we suppose to see out playlist");
-            System.out.println("playlist button pressed");
-        }
-    }
+	public class PlaylistEvent implements ActionListener {
+		public void actionPerformed(ActionEvent ex) {
+			playlistInfo.setText("Here we suppose to see out playlist");
+			System.out.println("playlist button pressed");
+		}
+	}
 
-    public static void main(String args[]) {
-        AudioPlayer Core = new AudioPlayer();
-        Core.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Core.setSize(400, 200);
-        Core.setResizable(false);
-        Core.setVisible(true);
-        Core.setTitle("Audio Player");
+	public static void main(String args[]) {
+		AudioPlayer Core = new AudioPlayer();
+		Core.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Core.setSize(400, 200);
+		Core.setLocationRelativeTo(null);
+		Core.setResizable(false);
+		Core.setVisible(true);
+		Core.setTitle("Audio Player");
 
-    }
+	}
 }
-
 
 
 
